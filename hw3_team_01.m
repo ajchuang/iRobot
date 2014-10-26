074e120b52a368987e84f72f37ba8ff65a658db8
ajchuang <alfred.jenchieh.huang@gmail.com>
2014-10-26 18:07:02 -0400

    all

--- hw3_team_01.m
+++ hw3_team_01.m
@@ -73,7 +73,11 @@ function finalRad= hw3_team_01 (serPort)
     end
     
     % do bug to find the wall
-    bug_algo (serPort);
+    reset_bug_params ([4.0, 0.0]);
+    turn_to_target (serPort, [4.0, 0.0]);
+    bug_algo (serPort, [25, 49]);
+    
+    % set up walls
     wall_annotation ();
     
     while (true)
@@ -116,9 +120,41 @@ function turn_to_target (serPort, M)
     global c_TurnSpeed;
     
     theta = atan2 (M(2) - g_total_y_dist, M(1) - g_total_x_dist);
-    angle = rad2deg (((-1) * g_total_angle + theta)); 
+    angle = rad2deg (((-1) * g_total_angle + theta));
     
-    turnAngle (serPort, c_TurnSpeed, angle);
+    angle = mod (round(angle), 360);
+    
+    if (angle > 180)
+        angle = (180 - (angle - 180)) * (-1);
+    end 
+    
+    
+    %log
+    angle
+    
+    thisAngle = angle;
+    SetFwdVelAngVelCreate (serPort, 0.0, 0.0);
+    
+    while (true)
+        before = g_total_angle;
+        turnAngle (serPort, 0.15, thisAngle);
+        update_moving_stats (serPort);
+        after = g_total_angle;
+        
+        thisAngle = thisAngle - rad2deg (after - before);
+        thisAngle = mod (round(thisAngle), 360);
+        
+        if (thisAngle > 180)
+            thisAngle = (180 - (thisAngle - 180)) * (-1);
+        end
+        
+        if (abs (thisAngle) < 5)
+            break;
+        else
+            display (sprintf ('insufficient angle: %f', thisAngle));
+            continue;
+        end
+    end    
 end
 
 function go_to_target (serPort, M)
@@ -176,18 +212,15 @@ end
 
 function go_to_block (serPort, blk)
     
+    % translate the coordinates
+    coord_mat = translate_blk_to_coord (blk);
+    reset_bug_params (coord_mat);
     
-    M= translate_blk_to_coord (blk);
+    % turn to the target
+    turn_to_target (serPort, coord_mat);
     
-    blk
-    M
-    
-    turn_to_target (serPort, M);
-    go_to_target (serPort, M);
-    
-    % do the m-line
-    %while (true)
-    %end
+    % go to destination
+    bug_algo (serPort, blk);
 end
 
 function unexplored_blk= any_unexplored_blks ()
@@ -275,10 +308,21 @@ function wall_annotation ()
 end
 
 % to impl
-function reset_bug_params ()
+function reset_bug_params (dst)
+    global g_goal_point;
+    global g_found_box;
+    global g_contact_x_dist;
+    global g_contact_y_dist;
+
+    g_goal_dist         = dst; 
+    g_found_box         = false;
+    g_contact_x_dist    = 0.0;
+    g_contact_y_dist    = 0.0;
+    
+    reset_bumping_moving_status ();
 end
 
-function bug_algo (serPort)
+function bug_algo (serPort, dst_blk)
 
     % constants
     global c_SimMode;
@@ -295,11 +339,14 @@ function bug_algo (serPort)
     global c_CenterTurnAngle;
     global c_TurnSpeed;
     global c_TurnRadius;
+    global c_blk_wall;
+    global c_blk_explored;
 
     global g_found_box;
     global g_total_x_dist;
     global g_total_y_dist;
     global g_total_angle;
+    global g_map_matrix;
 
     % loop variables
     wallSensor  = false;
@@ -308,16 +355,9 @@ function bug_algo (serPort)
     p_bCenter   = false;
     p_bLeft     = false;
     p_ang       = 0;
-    
-    reset_bug_params ();
 
     % Start robot moving: go straight
     SetFwdVelAngVelCreate (serPort, c_SlowFwdVel, 0.0);
-
-    % hit the wall and stop - we disable this for HW2
-    %waitBump (serPort);
-    %BeepRoomba (serPort);
-    %SetFwdVelAngVelCreate (serPort, 0.0, 0.0);
 
     % Enter main loop: control cycle is about 2.3 (sec)
     while (true)
@@ -329,7 +369,7 @@ function bug_algo (serPort)
         update_moving_stats (serPort);
 
         % check if mission completed
-        if (checkMovingStats ())
+        if (checkMovingStats () || g_map_matrix (dst_blk(1), dst_blk(2)) ~= 1)
             display ('Found the end point - Stop!')
             break;
         end
@@ -355,7 +395,7 @@ function bug_algo (serPort)
         if (bumped)
             
             % set current location as 'obstacle'
-            update_current_map (2);
+            update_current_map (c_blk_wall);
 
             display ('bump into the wall');
 
@@ -385,7 +425,7 @@ function bug_algo (serPort)
                 elseif (wallSensor)
                     
                     % also update the current location as wall
-                    update_current_map (2);
+                    update_current_map (c_blk_wall);
 
                     % a minor optimization using Wall Sensor
                     SetFwdVelAngVelCreate (serPort, c_SlowFwdVel, 0.0);
@@ -394,7 +434,7 @@ function bug_algo (serPort)
                 end
             else
                 % also update the current location as traversed
-                update_current_map (0);
+                update_current_map (c_blk_explored);
             end
         end
 
@@ -402,7 +442,7 @@ function bug_algo (serPort)
         pause (c_LoopInteval);
     end
     
-    update_moving_stats ();
+    update_moving_stats (serPort);
     SetFwdVelAngVelCreate (serPort, 0.0, 0.0);
 end
 
@@ -505,7 +545,6 @@ function neverEnd= checkExitObstacle (serPort)
             
             % keep going
             SetFwdVelAngVelCreate (serPort, c_SlowFwdVel, 0);
-            % waitBump (serPort);
             return;
         end
     end 
@@ -664,12 +703,64 @@ end
 % TODO
 % there is a hard-coded constant
 function b_is_mline= is_mline ()
+
+    % current pos
+    global g_total_x_dist;
     global g_total_y_dist;
+    
+    % impact pos
+    global g_contact_x_dist;
+    global g_contact_y_dist;
+    
+    % destination
+    global g_goal_dist;
+    
     global c_MaxToleranceRadius;
     
     b_is_mline = false;
+    s = Inf;
+    dist = Inf;
     
-    if (abs (g_total_y_dist) < 0.05)
+    % log
+    tmp = g_goal_dist - [g_contact_x_dist, g_contact_y_dist];
+    
+    if (tmp (1) ~= 0 && tmp (2) ~= 0)
+        s = tmp (2) / tmp (1);
+    end
+    
+    if (s ~= Inf && s ~= 0)
+        c = (-1) * s * g_goal_dist (1) + g_goal_dist (2);
+        b = 1 / s * g_total_x_dist + g_total_y_dist;
+        
+        intersec_x = s / (1 + s .^ 2) * (b - c);
+        intersec_y = (s .^ 2) / ( 1 + s .^ 2) * (b - c) + c;
+        
+        %log 
+        s
+        b
+        c
+        intersec_x
+        intersec_y
+        g_goal_dist
+        g_total_x_dist        
+        g_total_y_dist
+        
+        dist = point_distance ([g_total_x_dist, g_total_y_dist], [intersec_x, intersec_y]);
+        
+        %log
+        dist
+    else
+        if (tmp (2)==0)
+            dist = abs (g_total_y_dist - g_goal_dist (2));
+        else
+            % s == inf
+            dist = abs (g_total_x_dist - g_goal_dist (1));
+        end
+    end
+    
+    display (sprintf ('current dist %f', dist));
+    
+    if (dist < 0.1)
         display ('m_line is found');
         b_is_mline = true;
         return;
@@ -684,8 +775,9 @@ function isTrue= closer_to_the_goal ()
     global g_contact_y_dist;
     global g_goal_dist;
     
-    dist_new = abs (g_total_x_dist - g_goal_dist);
-    dist_old = abs (g_contact_x_dist - g_goal_dist);
+    dist_old = point_distance ([g_contact_x_dist, g_contact_y_dist], g_goal_dist);
+    dist_new = point_distance ([g_total_x_dist, g_total_y_dist], g_goal_dist);
+
     display (sprintf ('closer_to_the_goal: new = %f, old = %f', dist_new, dist_old));
     
     % trick - to avoid failing to exit
@@ -709,9 +801,10 @@ function hit= hit_bumping_pt ()
     % set hit as false
     hit = false;
     
-    if (dist_after_bumping () > 0.2)
+    if (dist_after_bumping () > 0.3)
     
-        d = sqrt ((g_total_x_dist - g_contact_x_dist)^2 + (g_total_y_dist - g_contact_y_dist)^2);
+        d = sqrt ((g_total_x_dist - g_contact_x_dist) .^ 2 + ...
+                  (g_total_y_dist - g_contact_y_dist) .^ 2);
         
         distance_to_hit_point = d
         
@@ -726,7 +819,6 @@ end
 function dist= dist_after_bumping ()
 
     global g_abs_dsit_after_bump;
-    g_abs_dsit_after_bump
     dist = g_abs_dsit_after_bump;
 end
 
@@ -1032,7 +1124,7 @@ function init_bug ()
     global g_contact_x_dist;
     global g_contact_y_dist;
 
-    g_goal_dist         = 15.0;  % This is golden!
+    g_goal_dist         = [15.0, 0.0];
     g_found_box         = false;
     g_contact_x_dist    = 0.0;
     g_contact_y_dist    = 0.0;
@@ -1049,7 +1141,6 @@ function init_global ()
     
     % referred constants
     global c_map_dim;
-    
 
     % declare the map (50 x 50 array)
     g_map_matrix        = ones (c_map_dim);
@@ -1063,9 +1154,6 @@ end
 
 function init_world ()
 
-    % clear everything
-    clear all;
-    
     % do everything
     init_const ();
     init_global ();
