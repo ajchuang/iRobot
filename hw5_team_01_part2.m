cf667630b8eb5b3f7a661437e3b579341c242c66
memorystickduo <chenduochenduo@hotmail.com>
2014-12-06 16:20:25 -0500

    hw5_part2 upadate

--- hw5_team_01_part2.m
+++ hw5_team_01_part2.m
@@ -175,34 +175,34 @@ end
 
 function findDoor(serPort, line)
 
-	com  = camera()
-	turn  = 16
+    com  = camera()
+    turn  = 16
 
-	% line depends on which side the door is at
-	
+    % line depends on which side the door is at
+    
 
-	if (com(1) < line)
-		side = 1;
-	else
-		side = 0;
-	end
+    if (com(1) < line)
+        side = 1;
+    else
+        side = 0;
+    end
 
-	while (abs(com(1) - line) > 20)
-		SetFwdVelAngVelCreate (serPort, 0.0, 0.0);
+    while (abs(com(1) - line) > 20)
+        SetFwdVelAngVelCreate (serPort, 0.0, 0.0);
         if(com(1) < 160)
-        	if (side == 0)
-        		turn = turn/2;
-        		side = 1;
-        	end
-        	turn_to_target (serPort, turn);
-        	display('turned right!');
+            if (side == 0)
+                turn = turn/2;
+                side = 1;
+            end
+            turn_to_target (serPort, turn);
+            display('turned right!');
         else
-        	if (side == 1)
-        		turn = turn/2;
-        		side = 0;
-        	end
-        	turn_to_target (serPort, (-1) * turn);
-        	display('turned left!');
+            if (side == 1)
+                turn = turn/2;
+                side = 0;
+            end
+            turn_to_target (serPort, (-1) * turn);
+            display('turned left!');
         end
 
         com = camera();
@@ -213,10 +213,10 @@ end
 function line = determine_line(com_x, picture_width, ratio) %ratio > 0.5
 
     if (com_x < picture_width/2)
-		line = (1 - ratio) * picture_width
-	else
-		line  = ratio * picture_width
-	end
+        line = (1 - ratio) * picture_width
+    else
+        line  = ratio * picture_width
+    end
 end
 
 function prop= find_light (img)
